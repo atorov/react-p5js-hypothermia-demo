6893fbb991fed2b62f7f7f03e88c1419544ba0eb
Veselin <veselin.lk.petrov@gmail.com>
2019-05-23 10:04:17 +0300

    p5 sketch wrapper

---
+++ src/components/P5Wrapper/index.jsx
@@ -0,0 +1,42 @@
+import React, { memo, useEffect } from 'react'
+import PropTypes from 'prop-types'
+
+import { generate } from 'shortid'
+
+export default function (id = generate()) {
+    let canvas = null
+
+    function P5Wrapper({ sketch, dispatch, state }) {
+        // console.log(`::: P5Wrapper #${id} component has been re-rendered`)
+
+        const elementId = `p5-wrapper-${id}`
+
+        useEffect(() => {
+            canvas = new window.p5(sketch, elementId)
+            canvas.dispatch = dispatch
+            canvas.state = { ...state }
+
+            return () => canvas.remove()
+        }, [dispatch, elementId, sketch, state])
+
+        return <div id={elementId} className="section" />
+    }
+
+    P5Wrapper.propTypes = {
+        dispatch: PropTypes.func,
+        sketch: PropTypes.func,
+        state: PropTypes.object,
+    }
+
+    P5Wrapper.defaultProps = {
+        dispatch: () => { },
+        sketch: () => { },
+        state: {},
+    }
+
+    return memo(P5Wrapper, (_, nextProps) => {
+        canvas.state = { ...nextProps.state }
+
+        return true
+    })
+}
